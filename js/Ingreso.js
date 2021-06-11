3d233a28d280c52014638baa7d2f48f9db02358b
Cristian Carrillo <cristiancarrillo311087@gmail.com>
2021-06-11 08:30:36 -0400

    definiendo nuestras clases Dato, Ingreso y Egreso

---
+++ js/Ingreso.js
@@ -0,0 +1,16 @@
+// clase hija de Ingreso
+
+class Ingreso extends Dato{
+
+    static contadorIngresos=0;
+
+    constructor(descripcion, valor){
+
+        super(descripcion, valor);
+        this._id = ++Ingreso.contadorIngresos;
+    }
+
+    get id(){
+        return this._id;
+    }
+}
